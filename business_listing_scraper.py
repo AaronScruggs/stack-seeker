7332180d260ea6bdd56633604a4135791d53698c
Aaron Scruggs <aarondscruggs@gmail.com>
2017-05-17 17:45:32 -0700

    Add delay for full page load

--- business_listing_scraper.py
+++ business_listing_scraper.py
@@ -10,22 +10,17 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.proxy import *
 import re
 from stack_scraper import StackScraper
+import time
 
 
 class BusinessListingScraper:
     def __init__(self,
-                 base_url='http://www.manta.com/mb_53_G2_CKV',
-                 term='computer_software',
-                 region='las_vegas_nv',
-                 first_page=1,
-                 stop_page=2):
-        self.base_url = base_url
-        self.term = term
-        self.region = region
-        self.start_page = first_page
-        self.stop_page = stop_page
+                 url,
+                 tech_term='django'):
         self.driver = None
+        self.url = url
         self.weblink_constant = '/api/v1/urlverify/http%3A%2F%2F'
+        self.tech_term = tech_term
 
     def init_selenium_scraper(self):
         myProxy = '119.237.211.19'
@@ -35,31 +30,59 @@ class BusinessListingScraper:
         profile.set_preference("network.proxy.http_port", 3128)
         profile.update_preferences()
         driver = webdriver.Firefox(firefox_profile=profile)
+        # driver.set_page_load_timeout(5)
         self.driver = driver
 
-    def request_html(self, page_num):
-        url = '/'.join([self.base_url, self.term, self.region]) + '?pg={}'.format(page_num)
-        self.driver.get(url)
-        return self.driver.page_source
+    # def request_html(self, page_num):
+    #     url = '/'.join([self.base_url, self.term, self.region]) + '?pg={}'.format(page_num)
+    #     self.driver.get(url)
+    #     print('title: ', self.driver.title)
+        # return self.driver.page_source
 
 
-    def get_website_list(self, content):
+    def lookup_website_list(self, website_list):
+        for website in website_list:
+            stack_scraper = StackScraper(self.tech_term, website, self.driver)
+            stack_scraper.run_scrape()
+
+    def connect_to_site(self):
+        self.init_selenium_scraper()
+        self.driver.get(self.url)
+
+    def get_website_list(self):
         # Take manta html content and return a list of business websites
-        soup = BeautifulSoup(content, 'html.parser')
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         hrefs = soup.find_all('a', href=re.compile(self.weblink_constant))
         links = [x['href'].strip(self.weblink_constant) for x in hrefs]
         return list(set(links))
 
-    def lookup_website_list(self, website_list):
-        pass
-
-
-    def process_companies(self):
-        # for each page: get html from manta, get list of business urls, run stack scraper on each
-
+    def process_page(self):
         self.init_selenium_scraper()
-        for page_num in range(self.start_page, self.stop_page):
-            html = self.request_html(page_num)
-            if html:
-                website_list = self.get_website_list(html)
+        for i in range(4):
+            try:
+                self.driver.get(self.url)
+            except TimeoutException:
+                print('timed out')
+
+            time.sleep(5)
+            website_list = self.get_website_list()
+            print(website_list)
+
+            if website_list:
                 self.lookup_website_list(website_list)
+                break
+            else:
+                continue
+        self.driver.quit()
+
+
+
+
+if __name__ == '__main__':
+    urls = ['http://www.manta.com/mb_53_G4_CKV/information_technology/las_vegas_nv?pg={}'.format(x)
+            for x in range(1, 2)]
+
+    for url in urls:
+        print(url)
+        bls = BusinessListingScraper(url)
+        bls.process_page()
--- stack_scraper.py
+++ stack_scraper.py
@@ -2,17 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from global_variables import proxy_list, request_headers
+import time
 
 
 class StackScraper:
     """
     Search a companies website stack for the given search term. Positive results are written to the record file.
     """
-    def __init__(self, tech_term, company_website, record_file='matching_companies.csv'):
+    def __init__(self, tech_term, company_website, driver, record_file='matching_companies.csv'):
         self.tech_term = tech_term.lower()
         self.base_url = 'https://builtwith.com/'
         self.company_website = company_website
         self.record_file = record_file
+        self.driver = driver
+        self.url = self.base_url + self.company_website
 
     def request_html(self):
         url = self.base_url + self.company_website
@@ -22,23 +25,29 @@ class StackScraper:
                 return r
         return None
 
+    def check_term(self, items):
+        for item in items:
+            if self.tech_term in item:
+                return True
+        return False
+
     def search_content(self, content):
         soup = BeautifulSoup(content, 'html.parser')
         items = soup.find_all('div', class_='techItem')
-        for item in items:
-            lower_text = item.text.lower()
-            if self.tech_term in lower_text:
-                return self.tech_term
-        return False
+        text_items = [item.find('h3').text.lower() for item in items]
+        return text_items
+
 
     def run_scrape(self):
-        r = self.request_html()
-        if r:
-            result = self.search_content(r.content)
-        else:
-            result = 'Connection failed'
+        # r = self.request_html()
+        self.driver.get(self.url)
+        time.sleep(5)
+        results = self.search_content(self.driver.page_source)
+        term_found = self.check_term(results)
+        results = [term_found] + results
 
         with open(self.record_file, 'a') as file:
             csvwriter = csv.writer(file)
-            csvwriter.writerow([self.company_website, result])
+            data = [self.company_website] + results
+            csvwriter.writerow(data)
 
